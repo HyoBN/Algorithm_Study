040663a529f2c12eb29bfd1b2b881235ced2f34e
양효빈 <qls0786@naver.com>
2021-04-02 14:19:35 +0000

    입출력_문자열_getline()

--- BOJ_PS/boj_11719.cpp
+++ BOJ_PS/boj_11719.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
     string str;
     while(!cin.eof()){
-        getline(cin,str);
+        getline(cin,str); //애초에 getline()은 공백도 입력받아짐
         cout<<str<<endl;
     }
 	
