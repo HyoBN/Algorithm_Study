ec23266c1f3317048a631f3a6ca46d11c4c02ef6
양효빈 <qls0786@naver.com>
2021-04-05 14:28:44 +0000

    _____algorithm

---
+++ BOJ_PS/boj_2751.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n;
+    int arr[1000001];
+    int temp;
+    scanf("%d",&n);
+    
+    for(int i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+    
+    sort(arr,arr+n);
+    
+    for(int i=0;i<n;i++)
+        printf("%d\n",arr[i]);
+                
+    
+}
