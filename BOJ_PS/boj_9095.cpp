22272796e4deda2dd061025772126e49576b03a9
양효빈 <qls0786@naver.com>
2021-04-25 14:25:16 +0000

    DP_점화식 찾기

--- BOJ_PS/boj_9095.cpp
+++ BOJ_PS/boj_9095.cpp
@@ -8,7 +8,7 @@ int main()
     int n;
     int arr[12]={0,1,2,4,};
     for(int i=4;i<=11;i++)
-        arr[i]=arr[i-3]+arr[i-2]+arr[i-1];
+        arr[i] = arr[i-3] + arr[i-2] + arr[i-1];
     for(int i=0;i<t;i++){
         scanf("%d",&n);
         printf("%d\n",arr[n]);
