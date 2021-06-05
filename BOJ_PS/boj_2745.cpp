ecab4cecfed0be5e99f8a1d8a1c6f250b2fe4f43
양효빈 <qls0786@naver.com>
2021-06-05 00:52:40 +0000

    연습

---
+++ BOJ_PS/boj_2745.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int b;
+	string str;
+	long long sum=0;
+	cin>>str>>b;
+	int cur=1;
+	
+	for(int i=str.size()-1;i>=0;i--){
+		if(str[i] >='A'){
+			sum+=(str[i]-'A'+10)*cur;
+		}
+		else
+			sum+=(str[i]-'0')*cur;
+		cur*=b;
+			
+	}
+	cout<<sum;
+
+	
+}
