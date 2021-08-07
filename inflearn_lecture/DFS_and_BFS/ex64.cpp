067030d41a06bc2da19b5508c8926c1ece233ccf
양효빈 <qls0786@naver.com>
2021-08-07 06:52:57 +0000

    DFS - 경로 탐색 -  인접 행렬/인접리스트

--- inflearn_lecture/DFS_and_BFS/ex64.cpp
+++ inflearn_lecture/DFS_and_BFS/ex64.cpp
@@ -1,5 +1,8 @@
 // 문제 65. 방향 그래프가 주어지면 1번 정점에서 n번 정점으로 가는 모든 경로의 가지 수 출력.
 
+
+//sol 1. 인접 행렬 활용.
+
 #include <iostream>
 using namespace std;
 
@@ -36,3 +39,48 @@ int main()
 	
 	printf("%d",cnt);
 }
+
+//------------------------------------
+
+// sol 2. 인접 리스트 활용.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int visited[30],cnt=0,n;
+vector<int> map[21];
+
+void DFS(int v)
+{
+	if(v==n)
+		cnt++;
+	else{
+		for(int i=0;i<map[v].size();i++)
+		{
+			if(visited[map[v][i]]==0){
+				visited[map[v][i]]=1;
+				DFS(map[v][i]);
+				visited[map[v][i]]=0;
+			}
+		}
+	}
+}
+
+int main()
+{
+	int m, a,b;
+	scanf("%d %d",&n, &m);
+	for(int i=1;i<=m;i++)
+	{
+		scanf("%d %d",&a, &b);
+		map[a].push_back(b);
+	}
+	visited[1]=1;
+	DFS(1);
+	
+	printf("%d",cnt);
+}
+
+
+
