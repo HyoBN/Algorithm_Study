f62c05de937c7e4c5ed09ca430beeaa03ea24547
양효빈 <qls0786@naver.com>
2021-08-07 08:12:49 +0000

    DFS - 가중치 방향 그래프/최소비용 - 인접행렬/인접리스트

--- inflearn_lecture/DFS_and_BFS/ex67.cpp
+++ inflearn_lecture/DFS_and_BFS/ex67.cpp
@@ -1,5 +1,8 @@
 //문제 67. 가중치 방향 그래프가 주어지면 1번 정점에서 N번 정점으로 가는 최소비용을  출력하라.
 
+
+//-----------------------------
+// sol 1. 인접 행렬.
 #include <iostream>
 using namespace std;
 
@@ -41,5 +44,52 @@ int main()
 	
 	visited[1]=1;
 	DFS(1,0);
+	printf("%d",cost);
+}
+
+
+
+//---------------------------------------------
+// sol 2. 인접 리스트
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, cost=2147000000;
+
+vector<pair<int,int>> map[30];
+int visited[30];
+
+void DFS(int v, int sum)
+{
+	if(v==n){
+		if(sum<cost) cost=sum;
+	}
+	else{
+		for(int i=0;i<map[v].size();i++){
+			if(visited[map[v][i].first]==0){
+				visited[map[v][i].first]=1;
+				DFS(map[v][i].first,sum+map[v][i].second);
+				visited[map[v][i].first]=0;
+			}
+		}
+		
+	}
+}
+
+int main()
+{
+	int m,a,b,c;
+	scanf("%d %d",&n,&m);
+	
+	for(int i=0;i<m;i++)
+	{
+		scanf("%d %d %d",&a,&b,&c);
+		map[a].push_back(make_pair(b,c));
+	}
+	visited[1]=1;
+	DFS(1,0);
+	
 	printf("%d",cost);
 }
