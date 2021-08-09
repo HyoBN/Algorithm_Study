eb814298f35a618c034dbccb611cedc61581f178
양효빈 <qls0786@naver.com>
2021-08-09 13:46:17 +0000

    priority_queue - 최대힙/최소힙

--- inflearn_lecture/DFS_and_BFS/ex73.cpp
+++ inflearn_lecture/DFS_and_BFS/ex73.cpp
@@ -1,4 +1,6 @@
-// 문제 73. 자연수를 입력받다가 0이 입력되면 최댓값을 꺼내어 출력, 자료가 없으면 -1 출력. -1이 입력되면 종료.
+// 문제 73. 자연수를 입력받다가 0이 입력되면 최댓값(최솟값)을 꺼내어 출력, 자료가 없으면 -1 출력. -1이 입력되면 종료.
+// 최댓값 -> 최대힙 / 최솟값 -> 최소힙
+// 최소힙 - 자연수를 입력받아 -1을 곱하여 priority_queue에 넣어 정렬한 후, -1을 곱하여 최솟값 출력.
 
 #include <iostream>
 #include <queue>
@@ -15,10 +17,12 @@ int main()
 		{
 			if(pq.empty()) printf("-1\n");
 			else{
-				printf("%d\n",pq.top());
+				printf("%d\n",pq.top()); // 최대힙.
+				printf("%d\n",(-1)*pq.top()); // 최소힙.
 				pq.pop();
 			}
 		}
-		else pq.push(a);
+		else pq.push(a); //최대힙.
+		//else pq.push((-1)*a); 최소힙.
 	}
-}5
+}
