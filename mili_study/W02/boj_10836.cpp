5093211d64fe87295832c3197ce181f20d9ea281
양효빈 <qls0786@naver.com>
2021-08-13 13:54:04 +0000

    83점

--- mili_study/W02/boj_10836.cpp
+++ mili_study/W02/boj_10836.cpp
@@ -7,10 +7,11 @@ int main()
 {
 	int map[701][701];
 	vector<int> growth;
+	
 	int m,n,x;
-	int index;
 	
 	scanf("%d %d",&m,&n);
+	vector<int> total_growth(2*m-1);
 
 	for(int i=0;i<n;i++) //date
 	{
@@ -20,28 +21,19 @@ int main()
 			for(int k=0;k<x;k++) //0,1,2 각각의 개수만큼 반복
 				growth.push_back(j); // input : 1 2 3 -> growth={0,1,1,2,2,2}
 		}
-		
-		index=0;
-		for(int k=m-1;k>=0;k--) // 0번째 열(m-1행부터)
-			map[k][0]+=growth[index++];
-		
-		for(int k=1;k<m;k++) //0번째 행(1열부터)
-			map[0][k]+=growth[index++];
-	
-		index=m; // (1,1) ~ (m-1,m-1)는 (0,1) ~ (0,m-1)값에 종속적이므로 growth 벡터의 m~끝까지 인덱스만 접근하면 됨.
-		
-        for(int p=1;p<m;p++){
-			for(int k=1;k<m;k++)
-				map[p][k]+=growth[index++];
-			index=m;
-		}
-		
+		for(int j=0;j<growth.size();j++)
+			total_growth[j]+=growth[j];
+
 		growth.clear();
 	}
-	
+
 	for(int i=0;i<m;i++){
-			for(int j=0;j<m;j++)
-				printf("%d ",map[i][j]+1); // 배열:0으로 초기화, 시작값:1
-			printf("\n");
+		for(int j=0;j<m;j++){
+			if(j==0)
+				printf("%d ",total_growth[m-i-1]+1);
+			else
+				printf("%d ",total_growth[j+m-1]+1);
+		}
+		printf("\n");
 	}
 }
