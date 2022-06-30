941f50c9a00d30b9a3f52e99ce6fc313c70e3651
HyoBin Yang <50162252+HyoBN@users.noreply.github.com>
2022-06-30 19:04:27 +0900

    ps

---
+++ BOJ_PS/boj_09372.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int t,n,m;
+	int a,b;
+	cin>>t;
+	while(t--){
+		cin>>n>>m;
+		while(m--)
+			cin>>a>>b;
+		cout<<n-1<<'\n';
+	}
+	return 0;
+}
