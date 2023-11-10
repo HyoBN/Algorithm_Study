06c035a737e4a4385b7e23c2b92e036eb4a20aa5
HyoBin Yang <50162252+HyoBN@users.noreply.github.com>
2023-11-10 16:55:10 +0900

    backTracking

---
+++ BOJ_PS/boj_15652.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int n,m;
+int arr[10];
+void dfs(int start, int cnt){
+    if(cnt==m){
+        for(int i=0;i<m;i++){
+            cout<<arr[i]<<' ';
+        }
+        cout<<'\n';
+    } else{
+        for(int i=start;i<=n;i++){
+            arr[cnt]=i;
+            dfs(i,cnt+1);
+        }
+    }
+}
+int main(){
+    cin>>n>>m;
+    dfs(1, 0);
+}
