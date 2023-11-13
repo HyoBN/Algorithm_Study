36cd42eec7797c51ddf64f352edb58ffc707b363
HyoBin Yang <50162252+HyoBN@users.noreply.github.com>
2023-11-13 14:30:32 +0900

    backTracking

---
+++ BOJ_PS/boj_15657.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n,m;
+int arr[10];
+vector<int> v;
+void dfs(int start, int cnt){
+    if(cnt>m) {
+        for (int i = 0; i < v.size(); i++) {
+            cout<<v[i]<<' ';
+        }
+        cout<<endl;
+        return;
+    }
+    for (int i = start; i < n; i++) {
+        v.push_back(arr[i]);
+        dfs(i, cnt + 1);
+        v.pop_back();
+    }
+}
+
+int main(){
+    cin>>n>>m;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    sort(arr,arr+n);
+    dfs(0,1);
+
+}
