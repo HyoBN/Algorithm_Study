31ddccf2d46f35ee8a1c0cf90c28a1aabaf5e418
HyoBin Yang <50162252+HyoBN@users.noreply.github.com>
2024-04-01 17:47:50 +0900

    backTracking

---
+++ BOJ_PS/boj_15663.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n,m;
+vector<int> v;
+int output[8];
+bool visited[8];
+
+void backTracking(int cnt){
+    if(cnt==m){
+        for (int i = 0; i < m; i++) {
+            cout<<output[i]<<' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    int chk=0;
+    for (int i = 0; i < v.size(); i++) {
+        if(!visited[i] && v[i] != chk) {
+            output[cnt] = v[i];
+            chk=output[cnt];
+            visited[i]=true;
+            backTracking(cnt + 1);
+            visited[i]=false;
+        }
+    }
+}
+
+int main(){
+    cin>>n>>m;
+    for (int i = 0; i < n; i++) {
+        int tmp;
+        cin >> tmp;
+        v.push_back(tmp);
+    }
+    sort(v.begin(), v.end());
+    backTracking(0);
+}
